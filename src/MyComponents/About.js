89e57a4939b54311e7bb5a8bb55a94077fd5c401
Abhishek <abhishekkhokhar1408@gmail.com>
2022-01-25 20:46:29 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about section
+            <p>This is a fully functional TodoList for your day to day activities.</p>
+        </div>
+    )
+}
